d2ef2977127d1c8d85a92993516e68099f5e3ba7
Victor Avila <avilavictor1570@gmail.com>
2020-08-05 16:31:13 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
   end
   
   def execute_transaction
-    if @status == "pending" && sender.valid? && sender.balance > amount
+    if @status == "pending" && sender.valid?
       @sender.balance = @sender.balance - amount
       @receiver.balance = @receiver.balance + amount
       @status = "complete"
