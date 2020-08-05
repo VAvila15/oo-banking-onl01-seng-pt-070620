fa6064087ac2227018ef9938a1f68f4ff163788a
Victor Avila <avilavictor1570@gmail.com>
2020-08-05 16:32:39 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,7 @@ class Transfer
       @receiver.balance = @receiver.balance + amount
       @status = "complete"
     else
-      @status = "rejeceted"
+      @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
   end
